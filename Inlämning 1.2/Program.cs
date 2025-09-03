be2ec07dab9109409e4333f6102b258adb15ef01
osla08001 <osla08001@E-PW04SMBR>
2025-09-03 13:00:52 +0200

    Add project files.

---
+++ Inlämning 1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gör bakgrunden till mörkblå
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            //Gör texten till Vit
+            Console.ForegroundColor = ConsoleColor.White;
+            //Skriver ut text
+            Console.WriteLine("Oh no! Looks like you've gotten an error.");
+            Console.WriteLine(":/");
+           
+
+        }
+    }
+}
